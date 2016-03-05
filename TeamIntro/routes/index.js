35e0c8493876f2115cd4082e242e9518d7dd49dc
pcdqc <pcdqc@163.com>
2016-03-05 20:53:43 +0800

    node.express

---
+++ TeamIntro/routes/index.js
@@ -0,0 +1,24 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+module.exports = function(app) {
+  app.get('/', function (req, res) {
+    res.render('index', { title: 'IFE.FrontEnd' });
+  });
+  app.get('/SU', function (req, res) {
+  	res.sendfile('su.html');
+  });
+  // app.get('/Fido', function (req, res) {
+  // 	res.render('index', { title: 'Fido' });
+  // });
+  // app.get('/Cat', function (req, res) {
+  // 	res.render('index', { title: 'Cat' });
+  // });
+  // app.get('/WeiYu', function (req, res) {
+  // 	res.render('index', { title: 'WeiYu' });
+  // });
+  // app.get('/chuan', function (req, res) {
+  // 	res.render('index', { title: 'chuan' });
+  // });
+};
