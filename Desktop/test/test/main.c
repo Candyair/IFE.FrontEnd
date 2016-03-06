cf28a266bc6c4d0a5aa0ddb468635a8365afc6eb
zhouying <786551136@qq.com>
2016-03-06 18:11:16 +0800

    fido's intro

---
+++ Desktop/test/test/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  test
+//
+//  Created by zhouying on 15/10/27.
+//  Copyright (c) 2015年 zhouying. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
