d50951163bd1d3e9f1133ab843d1359f11152223
zhouying <786551136@qq.com>
2016-03-13 21:14:53 +0800

    change style

--- TeamIntro/public/Fido/js/Chow.js
+++ TeamIntro/public/Fido/js/Chow.js
@@ -13,37 +13,23 @@ window.onload=function(){
 			}
 			else if(temp=="false"){		
 				status.attributes.status.value="true";
-				status.innerHTML="&#xe7fa;"
+				status.innerHTML="&#xe61d;"
 				audio.play();
 				return;
 			}	
 		}
 	}
 	playMusic();
+	
+	var stroyboard = document.getElementById('Stroyboard');
+	stroyboard.addEventListener("mousemove",function(){
+		e = event||window.event;
+		var offsetX = -(e.clientX/100).toFixed(10);
+		var offsetY = -(e.clientY/10).toFixed(10);
+		this.style.webkitTransform =  'translate3d('+offsetX+'%,'+offsetY+'px,0)';
+	},false);;
 
-	function Bubble(initX,initY,initSize){
-		var offsetX	=	Math.random();
-		var offsetY	=	Math.random();
-		var origin	= {
-			X:initX,
-			Y:initY,
-			size:initSize,
-		};
-		this.translate = function(){
-		this.origin.X+=offsetX+'px';
-		this.origin.Y+=offsetY+'px';
-	}
-}
-		var bubble = new Bubble(5,5,5);
-		var stroyboard = document.getElementById('Stroyboard');
-		stroyboard.addEventListener("mousemove",function(){
-			e = event||window.event;
-			var offsetX = -(e.clientX/100).toFixed(10);
-			var offsetY = -(e.clientY/10).toFixed(10);
-			this.style.webkitTransform =  'translate3d('+offsetX+'%,'+offsetY+'px,0)';
-		},false);;
-		
-		
+
 
 
 
