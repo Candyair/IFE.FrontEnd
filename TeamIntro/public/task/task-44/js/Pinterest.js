0035b898e8d1de3fc4c3fb3973793a9126e0fced
陈慕杰 <642163903@qq.com>
2016-04-13 16:01:19 +0800

    modify task-44

--- TeamIntro/public/task/task-44/js/Pinterest.js
+++ TeamIntro/public/task/task-44/js/Pinterest.js
@@ -1,26 +1,40 @@
 ;(function(w,d,$,undefined){
+	//Í¼Æ¬×´Ì¬
+	var ImgStatus = {
+		flag : false,
+		startX : 0,
+		startY : 0,
+		left : 0,
+		top : 0
+	}
+	//ÆÙ²¼Á÷ÊµÀı
 	var Pinterest = function(elem, config){
 		this.$elem = elem;
 		this.$imgs = this.$elem.find('img');
+		this.images = [];
 		this.config = config;
 		this.streams = [];
 		this.line = config.line ? config.line : 4;
 		this.gap = config.gap ? config.gap : 15;
 		this.width = config.width ? config.width : this.getWidth();
 
-		//å›¾ç‰‡å…¨å±æ˜¾ç¤º
-		this.$elem.on('click','img',this.fullScreen);
+		//Í¼Æ¬È«ÆÁÏÔÊ¾
+		this.$elem.on('click','img', this.fullScreen);
+		$(d).on('click','.priterest-full-img', this.fullFigure);
+		$(d).on('click','.priterest-shrink-btn', this.shrinkImg);
+		$(d).on('mousedown','.priterest-big-img', this.dragImg);
+		$(d).on('mousemove','.priterest-big-img', this.moveImg);
+		$(d).on('mouseup','.priterest-big-img', this.dropImg);
 	}
 
 	Pinterest.prototype = {
-
-		//å¾—åˆ°æ¯åˆ—å®½åº¦å’Œå›¾ç‰‡å®½åº¦
+		//µÃµ½Ã¿ÁĞ¿í¶ÈºÍÍ¼Æ¬¿í¶È
 		getWidth: function(){
 			var width = this.$elem.width();
 			var index = this.line + 1;
 			return width ? (width - (this.gap * index))/this.line : 200;
 		},
-		//å¾—åˆ°å½“å‰æœ€çŸ­çš„åˆ—
+		//µÃµ½µ±Ç°×î¶ÌµÄÁĞ
 		getMinStreams: function(){
 			var that = this;
 			var min = that.streams[0].height();
@@ -31,51 +45,128 @@
 					min = height;
 					index = i;
 				}
-			}
+			};
 			return index;
 		},
-		//åˆå§‹åŒ–
+		//³õÊ¼»¯
 		init: function(){
 			var that = this;
 			for(var i=0; i<this.line; i++){
 				that.streams.push($("<div class='pinterest-stream' style='width:"+ that.width +"px; margin-left:" + that.gap + "px; margin-bottom:" + that.gap + "px; display:inline-block; vertical-align: top'></div>"));
-				that.streams[i].appendTo(that.$elem);
-			}
-			that.$imgs.css("margin-top", that.gap + 'px')
+			 	that.streams[i].appendTo(this.$elem);
+			};
+			that.$imgs.css({
+				"margin-top" : that.gap + 'px',
+				"width" : that.width + 'px'
+			});
 			$.each(that.$imgs, function(i, v){
-				that.$imgs.eq(i).width(that.width);
 				var index = that.getMinStreams();
 				that.$imgs.eq(i).appendTo(that.streams[index]);
-			})
+			});
+
 		},
-		//å¢åŠ å›¾ç‰‡
-		add: function( src ){
-			var img = $("<img src=" + src + " style='width:"+ this.width +"px; margin-top:" + this.gap + "px;' />");
-			this.$imgs.push(img[0]);
+		loading: function(){
+			this.$elem.hide();
+		},
+		loadComplete: function(){
+			this.$elem.show();
+		},
+		//Ôö¼ÓÍ¼Æ¬
+		add: function( elem ){
+			var that = this;
+			var $elem = $(elem);
+			$elem.css({
+				"margin-top" : that.gap + 'px',
+				"width" : that.width + 'px'
+			});
+			this.$imgs.push($elem[0]);
 			var index = this.getMinStreams();
-			img.appendTo(this.streams[index]);
+			$elem.appendTo(this.streams[index]);
 		},
-		//ç§»é™¤å›¾ç‰‡
+		//ÒÆ³ıÍ¼Æ¬
 		remove: function( index ){
 			index = parseInt(index);
 			if( index === undefined ){ this.$elem.children().remove(); return false; }
 			var i = index < 0 ? (index + this.$imgs.length) : index;		
 			this.$imgs.eq(i-1).remove();		
 		},
-		//å…¨å±æ˜¾ç¤ºå›¾ç‰‡
+		//È«ÆÁÏÔÊ¾Í¼Æ¬
 		fullScreen: function(){
 			var $wrap = $("<div class='priterest-wrap' style='width: 100%;height: 100%;left:0;top:0;position: fixed;'></div>");
 			var $fade = $("<div class='priterest-fade' style='width: 100%;height: 100%;background-color: #000;opacity:.8;z-index: 9999;'></div>");
-			var $full_img = $("<img src="+ this.src +" style='z-index:10000;position:absolute;left:50%;top:50%;transform:translate(-50%,-50%);max-height:80%;max-width:80%;'/>");
+			var $full_img = $("<img src="+ this.src +" style='z-index:10000;position:absolute;left:50%;top:50%;transform:translate(-50%,-50%);max-height:80%;max-width:80%;cursor:zoom-in;' class='priterest-full-img'/>");
+			var $shrink_btn = $("<a style='width:100%;height:70px;background-color:rgba(0,0,0,.35);cursor:zoom-out;position:absolute;z-index:10001;top:0;display:none;' class='priterest-shrink-btn'></a>")
 			$fade.click(function(){
 				$wrap.remove();
 			});
 			$wrap.append($fade);
 			$wrap.append($full_img);
+			$wrap.append($shrink_btn);
 			$wrap.appendTo('body');
+		},
+		//µã»÷ÏÔÊ¾ÍêÕûÍ¼Æ¬
+		fullFigure: function(){
+			if($(this).css('max-height') !== 'none'){
+				$(this).css({
+					"max-height" : "none",
+					"max-width" : "none",
+					"cursor": setCursor('grab')
+				}).addClass('priterest-big-img');
+				$('.priterest-shrink-btn').show();
+			}
+		},
+		//µã»÷ËõĞ¡Í¼Æ¬
+		shrinkImg: function(){
+			$('.priterest-full-img').css({
+				"max-height" : "80%",
+				"max-width" : "80%",
+				"left" : "50%",
+				"top" : "50%",
+				"cursor" : 'zoom-in'
+			}).removeClass('priterest-big-img');
+			$(this).hide();
+		},
+		//ÍÏ×§Í¼Æ¬
+		dragImg: function(event){
+			event.preventDefault();
+			$(this).css({
+				"cursor":  setCursor('grabbing')
+			})
+			//ÉèÖÃÍ¼Æ¬³õÊ¼×´Ì¬
+			ImgStatus.flag = true;
+			ImgStatus.startX = event.pageX;
+			ImgStatus.startY = event.pageY;
+			ImgStatus.left = parseInt(w.getComputedStyle(this , null)['left']);
+			ImgStatus.top = parseInt(w.getComputedStyle(this , null)['top']);
+		},
+		//ÒÆ¶¯ÊÓ¿ÚÏÔÊ¾µÄÍ¼Æ¬
+		moveImg: function(event){
+			if(ImgStatus.flag){
+				this.style.left = (event.pageX - ImgStatus.startX + ImgStatus.left) + 'px';
+				this.style.top = (event.pageY - ImgStatus.startY + ImgStatus.top) + 'px';
+			}
+		},
+		//·ÅÏÂÍ¼Æ¬
+		dropImg: function(){
+			ImgStatus.flag = false;
+			$(this).css('cursor',setCursor('grab'));
 		}
-	}
-	
+	};
+	//ÉèÖÃcursor×¥ÊÖÊôĞÔ
+	(function(){
+		var v = navigator.userAgent;
+		var prefix = v.indexOf('AppleWebKit') > -1 ? "-webkit-" : v.indexOf('Gecko') > -1 && v.indexOf('KHTML') == -1 ? "-moz-" : v.indexOf('Presto') ? '' : 'IE';
+
+		return setCursor = function( prop ){
+			if(prefix === 'IE'){
+				return "move";
+			}
+			else {
+				return prefix + prop;
+			}
+		};
+	})();
+
 	$.fn.pinterest = function(option){
 		var config = arguments[0] ? arguments[0] : {};
 		var value = arguments[1];
